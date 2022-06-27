383241e67923c328b4c6c4ba5457f611d713a8da
mxxxnxx <mmm.mt.co.jp@gmail.com>
2022-06-27 18:34:14 +0900

    DONE

--- src/resources/js/component/CanvasContainer.js
+++ src/resources/js/component/CanvasContainer.js
@@ -1,12 +1,27 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { Physics } from "use-ammojs";
+import Photos from "./Photos";
 
 const CanvasContainer = () => {
     return (
         <>
-            <Canvas camera={{ position: [0, 0, 17], far: 50 }}>
-                <OrbitControls makeDefault />
+            <Canvas
+                camera={{
+                    fov: 75,
+                    near: 0.1,
+                    far: 1000,
+                    position: [0, 0, -800],
+                }}
+            >
+                <Suspense fallback={null}>
+                    <ambientLight intensity={0.29} />
+                    <Physics>
+                        <Photos />
+                        <OrbitControls makeDefault />
+                    </Physics>
+                </Suspense>
             </Canvas>
         </>
     );
