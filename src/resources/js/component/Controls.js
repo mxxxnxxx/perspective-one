e9a7c309ba288fab104470714b763d1d2e30b477
mxxxnxx <mmm.mt.co.jp@gmail.com>
2022-06-27 19:07:42 +0900

    写真レンダー

--- src/resources/js/component/Controls.js
+++ src/resources/js/component/Controls.js
@@ -1,46 +1,45 @@
-// import { useEffect, useState } from "react";
-// import * as THREE from "three";
+import { useEffect, useState } from "react";
 
-// const keys = {
-//     ArrowUp: "forward",
-//     ArrowDown: "backward",
-//     ArrowLeft: "left",
-//     ArrowRight: "right",
-//     KeyA: "left",
-//     KeyD: "right",
-//     KeyW: "forward",
-//     KeyS: "backward",
-// };
+const keys = {
+    ArrowUp: "up",
+    ArrowDown: "down",
+    ArrowLeft: "left",
+    ArrowRight: "right",
+    KeyA: "up",
+    KeyD: "down",
+    KeyW: "forward",
+    KeyS: "backward",
+};
 
-// const moveFieldByKey = (key) => keys[key];
+const moveFieldByKey = (key) => keys[key];
 
-// const Controls = () => {
-//     const [movement, setMovement] = useState({
-//         forward: false,
-//         backward: false,
-//         left: false,
-//         right: false,
-//         up: false,
-//         down: false,
-//     });
-//     const handleMouseMove = (e) => {};
+const Controls = () => {
+    const [movement, setMovement] = useState({
+        forward: false,
+        backward: false,
+        left: false,
+        right: false,
+        up: false,
+        down: false,
+    });
+    const handleMouseMove = (e) => {};
 
-//     useEffect(() => {
-//         const handleKeyDown = (e) =>
-//             setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: true }));
-//         const handleKeyUp = (e) =>
-//             setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: false }));
-//         document.addEventListener("keydown", handleKeyDown);
-//         document.addEventListener("keyup", handleKeyUp);
-//         document.addEventListener("pointermove", handleMouseMove);
+    useEffect(() => {
+        const handleKeyDown = (e) =>
+            setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: true }));
+        const handleKeyUp = (e) =>
+            setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: false }));
+        document.addEventListener("keydown", handleKeyDown);
+        document.addEventListener("keyup", handleKeyUp);
+        document.addEventListener("pointermove", handleMouseMove);
 
-//         return () => {
-//             document.removeEventListener("keydown", handleKeyDown);
-//             document.removeEventListener("keyup", handleKeyUp);
-//             document.removeEventListener("pointermove", handleMouseMove);
-//         };
-//     }, []);
-//     return movement;
-// };
+        return () => {
+            document.removeEventListener("keydown", handleKeyDown);
+            document.removeEventListener("keyup", handleKeyUp);
+            document.removeEventListener("pointermove", handleMouseMove);
+        };
+    }, []);
+    return movement;
+};
 
-// export default Controls;
+export default Controls;
