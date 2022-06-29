219b4850e00762303bfe5806dd1853089dc323a0
mxxxnxx <mmm.mt.co.jp@gmail.com>
2022-06-29 21:50:07 +0900

    test

--- src/resources/js/component/Loading.jsx
+++ src/resources/js/component/Loading.jsx
@@ -1,10 +1,10 @@
 import { useProgress } from "@react-three/drei";
 const Loading = () => {
-    const { progress } = useProgress();
-    return (
-        <div>
+    const { active, progress } = useProgress();
+    return active ? (
+        <div className="loader">
             <span>{Math.floor(progress)}%</span>
         </div>
-    );
+    ) : null;
 };
 export default Loading;
